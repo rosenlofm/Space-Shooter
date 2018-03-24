a49299f8bc01f3faa530c84ab9c51661e5e15b1f
markrose <rosenlofm@gmail.com>
2018-03-24 17:22:22 -0400

    Improved Movement
    The ship now moves to all of the enemies after clicking on the Confirm Code button, but there is a bug. The ship shoots only at the leftmost and rightmost enemies, and it shoots 3 times at the leftmost enemy (instead of directing the 2nd and 3rd shot at the 2nd and 3rd enemy).

--- Assets/Scripts/Controller Scripts/LevelOneController.cs
+++ Assets/Scripts/Controller Scripts/LevelOneController.cs
@@ -4,54 +4,21 @@ using UnityEngine;
 
 public class LevelOneController : MonoBehaviour
 {
-    //      ************       //
-    // Evasive Maneuver Script //
-    private float targetManeuver;
-    public float dodge = 5f;
-    public float smoothing = 7.5f;
-    public float tilt = 10f;
-
-    public Vector2 startWait;
-    public Vector2 maneuverTime;
-    public Vector2 maneuverWait;
-    public Boundary boundary;
-
-    // enemy ship flies toward player
-    //private Transform playerTransform;
-    private float currentSpeed;
-
     public Transform playerTransform;
-    private Transform enemyTransform;
-    // Evasive Maneuver Script //
-    //      ************       //
 
+    private float thrust = 3;
 
-    //enum Waves { Zero, One, Two, Three };
-    //Waves wave = Waves.Zero;
+//**** each wave and the wave's enemy information ****//
+    private enum Waves { One, Two, Three }
+    Waves wave;
+    int enemyCount;
+    private bool canShoot; // controls when the ship can fire
 
-    public float thrust;
+    float[] waveOneEnemyLocations = { 3, 6, 9, 12 };
+    float[] waveTwoEnemyLocations = { 3, 6, 9, 12 };
+    float[] waveThreeEnemyLocations = { 3, 6, 9, 12 };
+//**** each wave and the wave's enemy information ****//
 
-    public Transform target;
-    public float speed;
-
-    Vector3 right = new Vector3(1f, 0, 0); //Vector in the direction you want to move in.
-    Vector3 left = new Vector3(-3f, 0, 0); //Vector " 
-
-    private float enemyPosition;
-    private float playerPosition;
-
-    float[] enemyXPositions = new float[10];
-    Vector3[] enemyPositions;
-    private int enemyNumber;  // enemy number associated with enemyPositions
-                                // i.e. enemyPositions[enemyNumber] = enemyPositions[i]
-
-
-
-
-
-
-    // commented out stuff I don't think I need for this controller script
-    // or if they're duplicates from the other class I copied over
 
     public GameObject[] hazards;
     //public static GameObject[] hazardsArray;
@@ -61,6 +28,7 @@ public class LevelOneController : MonoBehaviour
     //public float startWait;
     public float waveWait;
     //public int waveCount;
+
 
     float xPos = 3f;  // leftmost x position on playing field
     float zPos = 3f;  // bottom-most z position on playing field
@@ -93,44 +61,97 @@ public class LevelOneController : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        // NOTE: This way of automating movement works,
-        // but think about how to do it with a Rigidbody and FixedUpdate()
-        // for more realistic movement and quaternion rotation
-        
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        playerPosition = playerTransform.position.x;
 
-        if (moving == IsMoving.Left && playerTransform.position.x <= enemyPosition)  // enemy and player x positions are the same, so just fire
+        // NOTE: This way of automating movement works (but with bugs),
+        // think about how to do it with a Rigidbody and FixedUpdate()
+        // for more realistic movement and quaternion rotation
+
+        if (wave == Waves.One)
         {
-            Debug.Log("enemy is at the same x position as player");
-            // Instantiate shot here
-            moving = IsMoving.None;
-        }
-        else if (moving == IsMoving.Right && playerTransform.position.x >= enemyPosition)
-        {
-            Debug.Log("enemy was on the right but is at same x pos as player now");
-            // Instantiate shot here
-            moving = IsMoving.None;
+            if (moving == IsMoving.Left && playerTransform.position.x <= waveOneEnemyLocations[enemyCount])  // enemy and player x positions are the same, so just fire
+            {
+                moving = IsMoving.None;
+                StopAndShoot();
+
+                enemyCount++;
+                if (enemyCount < 4) { EnactWaveOne(enemyCount); }
+                else { /* all enemies are destroyed */ }
+            }
+            else if (moving == IsMoving.Right && playerTransform.position.x >= waveOneEnemyLocations[enemyCount])
+            {
+                moving = IsMoving.None;
+                StopAndShoot();
+
+                enemyCount++;
+                if (enemyCount < 4) { EnactWaveOne(enemyCount); }
+                else { /* all enemies are destroyed */ }
+            }
+
+            if (moving == IsMoving.Left) { playerTransform.Translate(Vector3.left * thrust * Time.deltaTime); }
+            if (moving == IsMoving.Right) { playerTransform.Translate(Vector3.right * thrust * Time.deltaTime); }
         }
 
-        if (moving == IsMoving.Left)
+        else if (wave == Waves.Two)
         {
-            playerTransform.Translate(Vector3.left * Time.deltaTime);
-            //StartCoroutine(SmoothMove(left, 1f)); // .0001f is the speed
+            if (moving == IsMoving.Left) 
+            {
+                if (playerTransform.position.x <= waveTwoEnemyLocations[enemyCount])
+                {
+                    moving = IsMoving.None;
+                    StopAndShoot();
+
+                    enemyCount++;
+                    if (enemyCount < 4) { EnactWaveTwo(enemyCount); }
+                    else { /* all enemies are destroyed */ }
+                }
+            }
+            else if (moving == IsMoving.Right) 
+            {
+                if(playerTransform.position.x >= waveTwoEnemyLocations[enemyCount])
+                {
+                    moving = IsMoving.None;
+                    StopAndShoot();
+
+                    enemyCount++;
+                    if (enemyCount < 4) { EnactWaveTwo(enemyCount); }
+                    else { /* all enemies are destroyed */ }
+                }
+            }
+
+            if (moving == IsMoving.Left) { playerTransform.Translate(Vector3.left * thrust * Time.deltaTime); }
+            if (moving == IsMoving.Right) { playerTransform.Translate(Vector3.right * thrust * Time.deltaTime); }
         }
-        if (moving == IsMoving.Right)
+
+
+    }
+
+    private void StopAndShoot()
+    {
+        Debug.Log("enemy is at the same x position as player");
+        // Instantiate shot here
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject
+        GetComponent<AudioSource>().Play();
+    }
+
+    private void SpawnWaveOne()
+    {
+         //yield return new WaitForSeconds(startWait);
+        for (int i = 0; i < hazardCount; i++)
         {
-            playerTransform.Translate(Vector3.right * Time.deltaTime);
-            //StartCoroutine(SmoothMove(right, 1f)); // .0001f is the speed
+            GameObject hazard = hazards[1];
+
+            Vector3 spawnPosition = new Vector3(xPos + xOffset, 0f, zPos + zOffset);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(hazard, spawnPosition, spawnRotation);
+
+            xOffset += 3;
+            if (xPos >= xMax && zPos < zMax) { xOffset = 3; zOffset += 3; }
         }
     }
 
-    //IEnumerator SpawnWaves()
-    private void SpawnWaveOne()
+    private void SpawnWaveTwo()
     {
-        enemyPositions = new Vector3[10];
-        enemyNumber = 0;  // enemyNumber should be set to 0 at start of each spawn
-
         //yield return new WaitForSeconds(startWait);
         for (int i = 0; i < hazardCount; i++)
         {
@@ -140,145 +161,74 @@ public class LevelOneController : MonoBehaviour
             Quaternion spawnRotation = Quaternion.identity;
             Instantiate(hazard, spawnPosition, spawnRotation);
 
-            enemyPositions[i] = new Vector3(xPos + xOffset, 0f, zPos + zOffset);
-            enemyXPositions[i] = (xPos + xOffset);
-
-            // NOTE: all three of these positions print the correct numbers
-            Debug.Log("[Vector3] enemyPositions[i] = " + enemyPositions[i]);
-            Debug.Log("[float] enemyXpositions[i] = " + enemyXPositions[i]);
-            Debug.Log("enemy " + i + "'s position is: " + (xPos + xOffset));
-
             xOffset += 3;
             if (xPos >= xMax && zPos < zMax) { xOffset = 3; zOffset += 3; }
         }
-
     }
-
-
 
     // Use this for initialization
     public void WaveOneAction()
     {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        enemyTransform = GameObject.FindGameObjectWithTag("Enemy").transform;
-        //rb = GetComponent<Rigidbody>();
+        wave = Waves.One;
+        enemyCount = 0;
+        SpawnWaveOne();
 
+        // here check if user input correctly solves coding problem
+        // and respond to incorrect input accordingly
+
+        EnactWaveOne(waveOneEnemyLocations[enemyCount]);
+    }
+
+    private void EnactWaveOne(float enemyLocation)
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     public void WaveTwoAction()
     {
+        wave = Waves.Two;
+        enemyCount = 0;
+        SpawnWaveTwo();
+
+        // here check if the user input correctly solves coding challenge
+        // and respond to incorrect input accordingly
+
+        EnactWaveTwo(waveTwoEnemyLocations[enemyCount]);
+    }
+
+    private void EnactWaveTwo(float enemyLocation)
+    {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
-        testVector = new Vector3(13, 0, 0);
-
-        Debug.Log("WaveTwoAction invoked...");
+        Debug.Log("****WaveTwoAction invoked...");
         Debug.Log("Player location is: " + playerTransform.position);
 
-        for (int i = 0; i < 4; i++)
-        {
-            if (i == 0)
-            {
-                enemyPosition = 3;
-            }
-            else
-            {
-                enemyPosition = i * 3;
-            }
-            //enemyPosition = i + 3;// enemyXPositions[i];
-            //enemyTransform.position = enemyPositions[i];
-            Debug.Log("Enemy's position: " + enemyPosition);
+        Debug.Log("Enemy's position: " + waveOneEnemyLocations);
 
-            //while (playerTransform.position.x < enemyPosition)
-            if (playerTransform.position.x < enemyPosition)
-            {//Using update to capture the keypress.
-                Debug.Log("player is farther right than enemy");
-                //playerTransform.Translate(Vector3.right);// * Time.deltaTime);
+        //while (playerTransform.position.x < enemyPosition)
+        if (playerTransform.position.x < enemyLocation)
+        {//Using update to capture the keypress.
+            Debug.Log("player is farther right than enemy");
+            //playerTransform.Translate(Vector3.right);// * Time.deltaTime);
 
-                moving = IsMoving.Right;  // tells Update() to call SmoothMove() Coroutine with a right direction
+            moving = IsMoving.Right;  // tells Update() to call SmoothMove() Coroutine with a right direction
 
-                //levelOnePlayerController.MoveRight(playerTransform, 3f);
-            }
-            //while (playerTransform.position.x > enemyPosition)
-            if (playerTransform.position.x > enemyPosition)
-            {
-                Debug.Log("player is farther right than enemy");
-                //playerTransform.Translate(Vector3.left);// * Time.deltaTime);
-
-                moving = IsMoving.Left;  // tells Update() to call SmoothMove() Coroutine with a left direction
-
-                //levelOnePlayerController.MoveLeft(playerTransform, 3f);
-            }
-
-            Wait();
+            //levelOnePlayerController.MoveRight(playerTransform, 3f);
         }
+        //while (playerTransform.position.x > enemyPosition)
+        if (playerTransform.position.x > enemyLocation)
+        {
+            Debug.Log("player is farther right than enemy");
+            //playerTransform.Translate(Vector3.left);// * Time.deltaTime);
 
+            moving = IsMoving.Left;  // tells Update() to call SmoothMove() Coroutine with a left direction
 
+        }
     }
 
     void WaveThreeAction()
     {
         /////////////////////////
     }
-
-    
-    private void FixedUpdate()
-    {
-        /*
-            rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
-            currentSpeed = rb.velocity.z;
-
-            float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
-            rb.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
-            rb.position = new Vector3
-                (
-                    Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
-                    0.0f,
-                    Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
-                );
-
-            rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
-            */
-    }
-    
-
-    IEnumerator SmoothMove(Vector3 direction, float speed)
-    {
-        /*
-        while (playerTransform.position.x != testVector.x)
-        {
-            playerTransform.Translate(new Vector3())
-        }
-        */
-        
-        Vector3 enemyPos = new Vector3(3f, 0f, 9f);
-        float startime = Time.time;
-        Vector3 start_pos = playerTransform.position; //Starting position.
-        Vector3 end_pos = enemyPositions[enemyNumber] + direction; //Ending position.
-        //enemyNumber++;
-
-        Debug.Log("end_pos = " + end_pos);
-        Debug.Log("enemy position = " + enemyPositions[enemyNumber]);
-        Debug.Log("player_pos = " + playerTransform.position);
-
-        /*
-        while (playerTransform.position.x != end_pos.x && ((Time.time - startime) * speed) < 1f)
-        {
-            float move = Mathf.Lerp(playerTransform.position.x, end_pos.x, (Time.time - startime) * speed);
-            playerTransform.position += direction * move *.001f;  // * .001f slows down the movement
-
-            yield return null;
-        }
-        */
-        
-
-        yield return new WaitForSeconds(5);
-    }
-
-    IEnumerator Wait()
-    {
-        Debug.Log("Before Waiting 7 seconds");
-        yield return new WaitForSeconds(7);
-        Debug.Log("After Waiting 7 Seconds");
-    }
-
+     
 }
