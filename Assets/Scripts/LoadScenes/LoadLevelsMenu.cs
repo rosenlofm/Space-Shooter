8416c6d11f864df25743b99b3ed9456ae14194f6
JiaqiWang319 <jqw319@gmail.com>
2018-03-28 12:26:52 -0400

    Connect to database
    load level menu ++get_user_password_from_database
    in sign in menu
    
    New: CreatenewUser ---in sign up panel

--- Assets/Scripts/LoadScenes/LoadLevelsMenu.cs
+++ Assets/Scripts/LoadScenes/LoadLevelsMenu.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Data;  
+using MySql.Data;
+using MySql.Data.MySqlClient; 
 
 public class LoadLevelsMenu : MonoBehaviour {
 
@@ -16,7 +19,24 @@ public class LoadLevelsMenu : MonoBehaviour {
         username = null;
         password = null;
     }
-
+	public void  get_password_from_database() {
+		string constr2 = "server=160.39.193.170;Database=mydb;User Id=root;password=onionst";
+		MySqlConnection mycon2 = new MySqlConnection(constr2);
+		mycon2.Open();
+		string query2 = "select password from Player where Username ="+ "'"+ userName.text +"';";
+		MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query2, mycon2);
+		DataSet ds = new DataSet();
+		dataAdapter.Fill(ds);
+		if ((ds.Tables[0].ToString()).Length == 0)
+			Debug.Log ("No user exists!");
+		else {
+			string db_password = (ds.Tables[0].Rows[0][0]).ToString();
+			if (db_password == userPassword.text)
+				Debug.Log ("Log in success!");
+			else
+				Debug.Log ("Password error!");
+		}
+	}
     // Update is called once per frame
     public void LoadLevelMenu()
     {
